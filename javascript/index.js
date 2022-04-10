5497f513052ff26987c8f3f3956b55440f483e84
Tomo-ja <tomonao.hj@gmail.com>
2022-04-10 12:51:47 -0700

    access api to bring random word

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,19 @@
 import { Tile } from "./Tile.js"
+import getNewWord from "./getNewWord.js";
 
-
+const data = getNewWord()
+data.then(res => console.log(res.word))
 const $field = document.getElementById("game_field")
-const keyBoardBtns = [...document.getElementsByClassName("key-board_key")]
 
+// add event on each key on screen when it's pressed
+const keyBoardBtns = [...document.getElementsByClassName("key-board_key")]
 keyBoardBtns.forEach(btn => {
 	btn.addEventListener("click", (e)=>{
 		e.preventDefault()
 		console.log(e.currentTarget.getAttribute("data-key"))
 	})
 });
+
 const initTiles = []
 const answer = "teach"
 
@@ -25,4 +29,3 @@ initTiles.forEach(tile => {
 	const $tileElement = tile.createDomElement()
 	$field.appendChild($tileElement)
 });
-
