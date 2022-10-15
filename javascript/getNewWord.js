937cf9f95c02fdffbe48ecd8537cb3f475417c5b
Tomo-ja <tomonao.hj@gmail.com>
2022-10-14 17:16:29 -0700

    update api key

--- javascript/getNewWord.js
+++ javascript/getNewWord.js
@@ -1,15 +1,20 @@
 export default function getNewWord(){
 	return new Promise(async (resolve) => {
-		const options = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
-				'X-RapidAPI-Key': '75fb1b8b83msh889f6c57c02b66bp1c2c6fjsn9dd3b4fe8318'
-			}
-		};
-	
-		const wordInfo = await fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5&partOfSpeech=verb ', options)
-		const wordObject = await wordInfo.json()
-		resolve(wordObject)
+		try{
+			const options = {
+				method: 'GET',
+				headers: {
+					'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+					// TODO: find a way to access env local without backend
+					'X-RapidAPI-Key': process.env.X_RapidAPI_Key
+				}
+			};
+		
+			const wordInfo = await fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5&partOfSpeech=verb ', options)
+			const wordObject = await wordInfo.json()
+			resolve(wordObject)
+		} catch {
+			resolve({word: 'money'})
+		}
 	})
 }
